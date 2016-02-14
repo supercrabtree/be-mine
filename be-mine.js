948c11d2ccd9b03720f711229649015c6ee3e243
George Crabtree <supercrabtree@googlemail.com>
2016-02-14 09:37:20 +0100

    Add everything

---
+++ be-mine.js
@@ -0,0 +1,11 @@
+var valentinesDayRect = document.querySelector('rect[data-date="2016-02-14"]') || 
+                        document.querySelector('rect[data-date="2017-02-14"]') ||
+                        document.querySelector('rect[data-date="2018-02-14"]');
+
+if (valentinesDayRect) {
+  var fill = valentinesDayRect.getAttribute('fill');
+
+  if (fill != '#eeeeee') {
+    valentinesDayRect.setAttribute('fill', '#e33134');
+  }
+}
